040b7d09d73d166b197773d0c81d2b08966845ce
Manonmani441 <36624028+Manonmani441@users.noreply.github.com>
2018-05-16 15:59:36 +0530

    Update largest no.py

--- Beginner Level/largest no.py
+++ Beginner Level/largest no.py
@@ -1,1 +1,13 @@
+a=int(input(enter the no1))
+b=int(input(enter the no2))
+c=int(input(enter the no3))
+if (a>b):
+  if (a>c):
+    print(a)
+if(b>a):
+  if(b>c):
+    print(b)
+else:      
+    print(c)
+    
 
